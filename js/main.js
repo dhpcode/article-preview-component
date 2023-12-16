1bb5ec3f925eb9617aa1652becb8c0c10233d761
davrsirius <davrsirius@gmail.com>
2023-12-16 23:53:59 +0500

    Add javascript to remove share block

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
+const elBody = document.querySelector('#body');
 const elShare = document.querySelector('.share-img');
 const elBottomBlock = document.querySelector('.article-container__bottom-bottom');
+const elShareBlockImg = document.querySelector('.share-block__img');
 
 elShare.addEventListener('click', function() {
     elBottomBlock.classList.add('article-container__bottom--open');
 });
+
+// elBody.addEventListener('onclick', function() {
+//     elBottomBlock.classList.remove('article-container__bottom--open');
+// });
+
+elShareBlockImg.addEventListener('click', function() {
+    elBottomBlock.classList.remove('article-container__bottom--open');
+});
+
